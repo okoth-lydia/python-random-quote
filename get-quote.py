372411a8c8a0a486b81df7865e0ec076286b978e
lydia okoth <lydiahokoth13@gmail.com>
2020-02-28 08:36:42 +0300

    Accept and save user input

--- get-quote.py
+++ get-quote.py
@@ -10,5 +10,24 @@ def important():
   rnd = random.randint(0, last)
   print("{0} {1} {2}".format(quotes[rnd],quotes[random.randint(0,last)],quotes[random.randint(0,last)]).replace("\n",""))
 
+def addquote():
+	# Open the file in append mode(add items)
+	f = open("quotes.txt","a")
+	# ASk user for a new input
+	quote = input("Enter a new quote:\n")
+	# write to file
+	f.write(quote+"\n")
+	# Make changes
+	f.flush()
+	#close the file
+	f.close()
+
+
 if __name__== "__main__":
-  important()
+	# input is always a string cast it to int
+	
+	choice = int(input("Choose an option:\n1.Print random Qoute\n2.Add quote\n"))
+	if  choice == 1:
+		important()
+	elif choice == 2:
+		addquote()
